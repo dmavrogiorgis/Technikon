cc89bc52181ba3159e316f81d6b3d12cefddb6fb
Dimitrios Mavrogiorgis <dimitriosm@ISIH.LOCAL>
2024-04-30 16:57:25 +0300

    Initial commit

---
+++ src/main/java/gr/scytalys/team3/Technikon/TechnikonApplication.java
@@ -0,0 +1,13 @@
+package gr.scytalys.team3.Technikon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnikonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnikonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gr/scytalys/team3/Technikon/TechnikonApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.scytalys.team3.Technikon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnikonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
