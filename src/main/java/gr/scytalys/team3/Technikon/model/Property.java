c572a0cc198746a427e0a8321d805b5e9daa1c07
antoniosk <antonioskalogiorgas@scytalys.com>
2024-05-17 15:09:27 +0300

    Merge pull request #5 from dmavrogiorgis/propertyOwnerFeatures
    Merge pull request #4 from dmavrogiorgis/master
    
    Signed-off-by: antoniosk <antonioskalogiorgas@scytalys.com>

---
+++ src/main/java/gr/scytalys/team3/Technikon/model/Property.java
@@ -0,0 +1,34 @@
+package gr.scytalys.team3.Technikon.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Property {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long propertyId;
+    @Column(unique = true)
+    private long propertyIN;
+    private String address;
+    private long yearOfConstruct;
+    @ManyToOne
+    @NotNull
+    private PropertyOwner propertyOwner;
+    private String picturePath;
+//    @Embedded
+//    private PropertyCoordinates propertyCoordinates;
+    private TypeOfProperty typeOfProperty;
+    @OneToMany(mappedBy = "property")
+    private List<RepairTask> repairTasks;
+
+    private boolean isActive = true;
+
+}
